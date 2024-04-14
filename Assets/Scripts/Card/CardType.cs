b9c5de916e5d32606e616159ed4e9b8e07e4c870
FLOYD993 <142295304+FLOYD993@users.noreply.github.com>
2024-04-14 15:28:24 +0800

    Revert "1"
    This reverts commit 568dfdd9dd62c82e10265801bee0c1086967ff42.

--- Assets/Scripts/Card/CardType.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CardType", menuName = "CardGame/Templates/Card Type", order = 0)]
-public class CardType : ScriptableObject
-{
-    public string TypeName;
-}
--- Assets/Scripts/Runtime/CharacterTemplate.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class CharacterTemplate : ScriptableObject
-{
-   public string Name;
-   public GameObject Prefab;
-}
